bb9484519a1494dbe9df7e219cf62f663243f220
Matthew Read <matthewreadbass@hotmail.co.uk>
2021-10-25 14:37:55 +0100

    Print Formatting number of decimal places

---
+++ PriceDisplay.java
@@ -0,0 +1,7 @@
+class PriceDisplay {
+  public static void main(String[] args) {
+    String item = "Coffee machine";
+    double price = 15.99;
+    System.out.printf("The price of the item %s is %.2f \n", item, price);
+  }
+}
